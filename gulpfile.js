49bc3f07e65bc8d9879d96e74329c3822d17f71b
Sam Deere <dev@colophonemes.com>
2015-08-21 15:06:46 +0100

    Add gulp build chain

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var through2 = require('through2');
+var rename = require("gulp-rename");
+
+
+// browserify build chain
+gulp.task('browserify', function () {
+  gulp.src('./app.js')
+  .pipe(through2.obj(function (file, enc, next){
+            browserify(file.path)
+                .bundle(function(err, res){
+                    // assumes file.contents is a Buffer
+                    file.contents = res;
+                    next(null, file);
+                });
+        }))
+  .pipe(gulp.dest('./js'));
+});
